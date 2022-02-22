20fd677dd000123bd855729c9e21d96f14901ea7
shukurulloh777 <100182391+shukurulloh777@users.noreply.github.com>
2022-02-22 09:53:54 +0500

    Add files via upload

---
+++ 5- uy ishi.py
@@ -0,0 +1,54 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Feb 22 07:08:59 2022
+
+@author: Dr Dilmurod
+"""
+
+# 5-dars uy ishi
+
+##1
+# bog={"olma","anor","nok"}
+##2
+# bog.add("kivi")
+##3
+# bog.remove("olma")
+# print(bog)
+
+##4
+# set1={10,20,30,40,50,60}
+# set2={30,40,50,60,70}
+# set3=set1.intersection(set2)
+# print(set3)
+
+
+##5
+# set1={10,20,30,40,50,60}
+# set2={30,40,50,60,70}
+# set3=set1.union(set2)
+# print(set3)
+
+##7
+# set1={10,20,30,40,50,60}
+# set2={30,40,50,60,70}
+# set3=set1.symmetric_difference(set2)
+# print(set3)
+
+##8
+# numbers={10,20,-7,86,999,1,68}
+# print(max(numbers))
+
+##9
+# numbers={10,20,-7,86,999,1,68}
+# print(min(numbers))
+
+
+
+
+
+
+
+
+
+
+
