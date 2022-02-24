e909ebff6788c9a62bba820ce81c5085f34f8d38
shukurulloh777 <100182391+shukurulloh777@users.noreply.github.com>
2022-02-24 08:46:43 +0500

    Add files via upload

--- 5- uy ishi.py
+++ 5- uy ishi.py
@@ -28,7 +28,7 @@ Created on Tue Feb 22 07:08:59 2022
 # set3=set1.union(set2)
 # print(set3)
 
-##7
+# #7
 # set1={10,20,30,40,50,60}
 # set2={30,40,50,60,70}
 # set3=set1.symmetric_difference(set2)
@@ -41,6 +41,50 @@ Created on Tue Feb 22 07:08:59 2022
 ##9
 # numbers={10,20,-7,86,999,1,68}
 # print(min(numbers))
+
+##10
+# number={10,25,-8,1,9.6,777}
+# for i in range(len(number)):
+#     print(min(number), end=", ")
+#     number.remove(min(number))
+
+
+##11
+# numbers={10,25,-8,19,6,777}
+# kvadrat=set()
+# for son in numbers:
+    # kvadrat.add(son**2)
+# print(kvadrat)
+
+##12
+# numbers={1,7,8,10,13,45,9}
+# toq=set()
+# for son in numbers:
+    # if son%2==1:
+        # toq.add(son)
+# print(toq)
+
+
+##13
+# numbers={1,7,8,10,13,45,9}
+# juft=set()
+# for son in numbers:
+#     if son%2==0:
+#         juft.add(son)
+# print(juft)
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
